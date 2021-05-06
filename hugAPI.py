6a4d042ab537a14d8626ba1e059c048b5043aa70
nclsbayona <nbayonaord@gmail.com>
2021-05-06 18:02:30 -0500

    new changes

--- hugAPI.py
+++ hugAPI.py
@@ -1,6 +1,6 @@
 import hug
 from random import randint, choice, shuffle
-import json
+from json import dumps
 
 """Simple API to test, some of the HUG capabilities"""
 
@@ -9,16 +9,16 @@ lib = dict()
 @hug.get(['/all','/'])
 def get_all():
     """Returns all available objects"""
-    return (json.dumps(lib, indent=4))
+    return eval(dumps(lib, indent=4))
 
 @hug.get('/only_one')
 def get_this(this: hug.types.number):
     """Returns an specific object"""
-    return json.dumps(lib[this])
+    return eval(dumps(lib[this], indent=4))
 
 @hug.get('/new')
 def get_new_this():
-    """Posts a new object"""
+    """Includes a new object"""
     key=len(lib)+1
     value=list(choice(str("BNnb1001191743"+"$%&/#@=?*, ")) for i in range(randint(16,32)))
     shuffle(value)
@@ -28,6 +28,7 @@ def get_new_this():
         key:value
     }
 
-@hug.post()
-def post_new():
+@hug.put('/new_one')
+def put_new(obj: hug.types.Chain(hug.types.number, hug.types.text)):
     """Usage of post"""
+    print(obj)
